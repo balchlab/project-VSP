634cf22b04a7569d81afc7586e5b8e002a6c2a94
gainullin <gainullin@gmail.com>
2016-10-07 20:32:07 -0700

    .

--- src/search_PROV_ExAC.py
+++ src/search_PROV_ExAC.py
@@ -28,29 +28,34 @@ VCF_HEADER = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO']
 #ENSP = "ENSP00000003084" #CFTR
 #ENSP = "ENSP00000269228" #NPC1
 ENSP = "ENSP00000262304" #PKD1
+ENSP = "ENSP00000262410" #MAPT
 #ENSG = "ENSG00000001626" #CFTR
 #ENSG = "ENSG00000141458" #NPC1
 ENSG = "ENSG00000008710" #PKD1
+ENSG = "ENSG00000186868" #ENSG
 # ENSG = "ENSG00000186868" #MAPT
 # ENSG = "ENSG00000272636" #Diagnostic - beginning of Chr17
 #ENST = "ENST00000003084" CFTR
 #ENST = 'ENST00000269228' #NPC1i
 ENST = "ENST00000262304" #PKD1
+ENST = "ENST00000262410" #MAPT
 
 
-GENE = "PKD1"
+GENE = "MAPT"
 FILENAME = "CFTR_PROV_extract.csv"
 FILENAME1 = "CFTR_PROVEANScores.csv"
-FILENAME2 = "PKD1_ExACScores.csv"
-FILENAME3 = "PKD1_MutPredScores.csv"
+FILENAME2 = "MAPT_ExACScores.csv"
+FILENAME3 = "MAPT_MutPredScores.csv"
 FILENAME4 = "dbNSFP_output.csv"
 FILENAME5 = "dbNSFP_extract.csv"
-FILENAME6 = "PKD1_ExACScores.csv"
+FILENAME6 = "MAPT_ExACScores.csv"
 #UniProt = "P13569" CFTR
 #UniProt = "O15118" #NPC1
 UniProt = "P98161" #PKD1
+UniProt = "P10636" #MAPT
 #Chr = '18' NPC1
-Chr = "16"
+Chr = "16" #PKD1
+Chr = "17"
 
 # change directory to working with DAta
 os.chdir("../Data/")
@@ -374,7 +379,7 @@ def lines(filename, Chr):
 
 
 def find_good_lines(fh):
-    #TODO: rewrite to account for Chr vs int variables and to reduce unnecessary searching. 
+    #TODO: rewrite to account for Chr vs int variables and to reduce unnecessary searching.
     i='0'
     for line in fh:
 
