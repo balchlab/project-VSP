8b29357f1580ed38b639a754b112aba028d224a5
vgainullin <vladimir@scripps.edu>
2016-11-22 10:35:09 -0800

    added names of low freq species for each aa pos

--- src/Score_alignments.py
+++ src/Score_alignments.py
@@ -3,6 +3,9 @@ import sys
 import collections, time
 
 
+ # open and read fasta files
+ # sperate names and store separately
+ #
 
 
 fasta = {}
@@ -20,28 +23,47 @@ with open("../Alignments/Rubisco.txt") as file_one:
         fasta[active_sequence_name].append(list(sequence))
 
 i = 0
-while i < 100:
+
+while i < 100: # this needs to be changed to total length of alignment
 
     str_list = []
     s = ""
-
+    low_freq_AAs = {}
     # first calculate frequencies of each amino acid in position
     # then iterate through these positions again
     # if found match and freq less than 50%
     # print out stats and name of key
     
     for key in fasta:
+
         for x in (fasta[key]):
 
-            s+=x[i]
+            s+=x[i-1]
 
-
+    print (s)
     Count = collections.Counter(s)
 
     for key, value in Count.items ():
-        print (i+1, key, value/35*100,'%')
+
+        #print (i+1, key, value/35*100,'%')
+
         if 50 > value/35*100:
-            print ("low")
+            #print ("low")
+            #print ("key:", key)
+
+            low_freq_AAs[key] = value/35*100
+
+    #print (low_freq_AAs)
+
+
+    for key in fasta:
+
+        for x in (fasta[key]):
+
+            if x[i-1] in low_freq_AAs:
+
+                print (i+1, x[i-1], low_freq_AAs[x[i-1]], key)
+
 
     i+=1
 
